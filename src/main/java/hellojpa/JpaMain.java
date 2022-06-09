1450b6dfae5d1512d3cf15afbf3838edf6b9708c
WoojinKim <dnwls488@naver.com>
2022-06-10 00:21:53 +0900

    DDL TEST

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class JpaMain {
@@ -18,18 +21,20 @@ public class JpaMain {
         tx.begin();
 
         try{
-            /*
-            Member member = new Member();
-            member.setId(2L);
-            member.setName("memberB");
+
+            Member member = new Member(1l, "member", 30, RoleType.ADMIN, Date.valueOf(LocalDate.now()), Date.valueOf(LocalDate.now()), "관리자");
+//            member.setId(4L);
+//            member.setName("memberB");
             em.persist(member);
-            */
+
+//            em.detach(member);
 
             /*
             Member findMember = em.find(Member.class, 1L);
             findMember.setName("memberB");
             */
 
+            /*
             List<Member> result = em.createQuery("select m from Member m", Member.class)
                     .setMaxResults(3)
                     .getResultList();
@@ -37,7 +42,7 @@ public class JpaMain {
             result.stream().forEach(member -> {
                 System.out.println("{ " + member.getId() + ", " + member.getName() + " }");
             });
-
+*/
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,9 +1,8 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 //@Table(name = "Member")
@@ -11,25 +10,33 @@ public class Member {
     @Id
     private Long id;
 
-//    @Column(name = "username")
-    private String name;
+    @Column(name = "name")
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
 
     public Member() {
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
+    public Member(Long id, String username, Integer age, RoleType roleType, Date createDate, Date lastModifiedDate, String description) {
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+        this.username = username;
+        this.age = age;
+        this.roleType = roleType;
+        this.createDate = createDate;
+        this.lastModifiedDate = lastModifiedDate;
+        this.description = description;
     }
 }
