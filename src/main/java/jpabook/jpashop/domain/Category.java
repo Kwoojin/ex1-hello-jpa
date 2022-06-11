eab91aa3082a5a3db0729550258db7c181ed9ca5
WoojinKim <dnwls488@naver.com>
2022-06-11 17:18:23 +0900

    cascade, orphanRemoval

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -23,7 +23,21 @@ public class Category extends BaseEntity {
     @JoinColumn(name = "parent_id", foreignKey = @ForeignKey(name = "fk_category_to_category"))
     private Category parent;
 
-    @OneToMany(mappedBy = "parent")
+    /**
+     * cascade
+     * - Parent - Child 의 LifeCycle 이 완전 돌일 할 때
+     * - Child 가 다른 Entity 에 연관 관계가 없고, Parent 에 완전 종속적일 때
+     * > 사용 가능
+     *
+     * orphanRemoval
+     * - 참조하는 곳이 하나일 때만 사용
+     * - 특정 엔티티가 개인 소유할 때 사용
+     * cascade = CascadeType.ALL, orphanRemoval = true
+     * 두 옵션을 모두 활성화 하면 부모 엔티티를 통해서,
+     * 자식의 생명 주기 관리 가능
+     */
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Category> child = new ArrayList<>();
 
     /**
