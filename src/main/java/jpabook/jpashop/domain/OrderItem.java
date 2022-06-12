60da6b7a1622fc939cf8b203101f57c89cdeb411
WoojinKim <dnwls488@naver.com>
2022-06-12 15:31:36 +0900

    SideEffect 방지로 인한 Setter 제거

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 import javax.persistence.*;
 
 @Entity
-@Getter @Setter
+@Getter
 @SequenceGenerator(
         name="orderItem_seq_generator",
         sequenceName = "orderItem_seq",
