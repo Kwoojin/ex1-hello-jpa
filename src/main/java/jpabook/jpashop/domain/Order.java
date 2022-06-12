2b147ba1e41c2016061db7d0363d4584aa27036b
WoojinKim <dnwls488@naver.com>
2022-06-12 15:31:29 +0900

    SideEffect 방지로 인한 Setter 제거

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Getter @Setter
+@Getter
 @SequenceGenerator(
         name="order_seq_generator",
         sequenceName = "order_seq",
